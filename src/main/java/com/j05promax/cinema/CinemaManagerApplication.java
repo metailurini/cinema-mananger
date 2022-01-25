34c3425a0811f0ea7277f113248921937e063053
Shane Nguyễn <97421922+danhngt@users.noreply.github.com>
2022-01-25 13:02:22 +0700

    Init base code (#1)
    Co-authored-by: shanenoi <shanenoi.org@gmail.com>

---
+++ src/main/java/com/j05promax/cinema/CinemaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.j05promax.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j05promax/cinema/CinemaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j05promax.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
