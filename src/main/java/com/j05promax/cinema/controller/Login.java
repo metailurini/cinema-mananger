9e326269b3fd1b0d2747fd0c958bb21d44a35357
Shánenoi <49734716+shanenoi@users.noreply.github.com>
2022-05-14 14:06:52 +0700

    get all film repo func (#11)
    * add login logic sample
    
    * add get all film repo

--- src/main/java/com/j05promax/cinema/controller/Login.java
+++ src/main/java/com/j05promax/cinema/controller/Login.java
@@ -1,16 +1,17 @@
-package com.j05promax.cinema.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class Login {
-    @GetMapping("/login")
-    public String login(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model) {
-        model.addAttribute("name", name);
-        return "login";
-    }
-}
+// package com.j05promax.cinema.controller;
+// 
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// 
+// @Controller
+// public class Login {
+//     @GetMapping("/login")
+//     public String login(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+//             Model model) {
+//         model.addAttribute("name", name);
+//         return "login";
+//     }
+// }
+// 
