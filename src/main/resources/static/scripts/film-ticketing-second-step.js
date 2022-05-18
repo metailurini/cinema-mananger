710e63ca0b83a190c5da58c7b0c4361c19ffe277
Bao Dang <danglequocbao001@gmail.com>
2022-05-19 01:19:29 +0700

    feat: UI step 2

--- src/main/resources/static/scripts/film-ticketing-second-step.js
+++ src/main/resources/static/scripts/film-ticketing-second-step.js
@@ -56,4 +56,11 @@ function initSeats() {
   }
 }
 
+var child = document.getElementById("child");
+var father = document.getElementById("father");
+
+for (let i = 0; i < 20; i++) {
+  father.appendChild(child.cloneNode(true));
+}
+
 initSeats();
