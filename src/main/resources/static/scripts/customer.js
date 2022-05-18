854c6fec4c9cef85fe40a23116058eb9215ad10e
Bao Dang <danglequocbao001@gmail.com>
2022-05-18 13:34:10 +0700

    fix: chore

--- src/main/resources/static/scripts/customer.js
+++ src/main/resources/static/scripts/customer.js
@@ -1,9 +1,9 @@
 var child = document.getElementById("child");
 var father = document.getElementById("father");
 
-for (let i = 0; i < 20; i++) {
-  father.appendChild(child.cloneNode(true));
-}
+// for (let i = 0; i < 20; i++) {
+//   father.appendChild(child.cloneNode(true));
+// }
 
 function more() {
   for (let i = 0; i < 20; i++) {
