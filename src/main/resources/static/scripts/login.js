703e19941249190771186fa0b880da4ceefc8915
shanenoi <shanenoi.org@gmail.com>
2022-05-18 22:12:39 +0700

    add toast login error

--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -9,3 +9,31 @@ if (toastTrigger) {
     toast.show();
   });
 }
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+
+if (decodeURI(getCookie('error')) != "") {
+  $("#toastContext")[0].innerHTML = decodeURI(getCookie('error')).replaceAll('+', ' ');
+  toastTrigger.click();
+  setCookie('error', '', 0.1);
+}
