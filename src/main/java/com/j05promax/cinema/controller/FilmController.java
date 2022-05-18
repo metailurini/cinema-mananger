ecf8f2656fcb5d0bb55edb56c8e20c7b8b19b597
ThaiNamHa-windows <hanamthai02@gmail.com>
2022-05-18 12:18:26 +0700

    show list customer/film

--- src/main/java/com/j05promax/cinema/controller/FilmController.java
+++ src/main/java/com/j05promax/cinema/controller/FilmController.java
@@ -3,7 +3,10 @@ package com.j05promax.cinema.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.j05promax.cinema.entity.Film;
 import com.j05promax.cinema.repo.PostgreSQLRepo;
 
 import org.springframework.stereotype.Controller;
@@ -35,14 +38,23 @@ public class FilmController {
 		}
 
 		model.addAttribute("staffName", "Staff's name");
-		PostgreSQLRepo postgreSQL = PostgreSQLRepo.getInstance();
+		PostgreSQLRepo repo = PostgreSQLRepo.getInstance();
 		int counted =0;
 		try {
-            counted = postgreSQL.Film.Count_Film_Active();
+            counted = repo.Film.Count_Film_Active();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 		model.addAttribute("countedFilm", counted);
+
+		List<Film> films = new ArrayList<>();
+
+		try{
+			films = repo.Film.GetAll();
+		}
+		catch(SQLException e){}
+
+		model.addAttribute("films", films);
 		return "film";
 	}
 
