f15483216c531aeab6e43e33d0e3255746c946ff
Bao Dang <danglequocbao001@gmail.com>
2022-05-18 17:20:41 +0700

    feat: test toast login

--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -1,1 +1,11 @@
 // alert("login");
+
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+  toastTrigger.addEventListener("click", () => {
+    const toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+  });
+}
