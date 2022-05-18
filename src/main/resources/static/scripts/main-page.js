3dc5a709025b87042985fd9c9412767b2a14115c
Bao Dang <danglequocbao001@gmail.com>
2022-05-18 11:44:09 +0700

    feat: dropdown logout menu & function

---
+++ src/main/resources/static/scripts/main-page.js
@@ -0,0 +1,20 @@
+function openCloseDropdownUser() {
+  var x = document.getElementById("openCloseDropdownUser");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function deleteAllCookies() {
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    var eqPos = cookie.indexOf("=");
+    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+  location.reload();
+}
