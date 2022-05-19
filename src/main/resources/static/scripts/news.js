310853e5b1bb0fb4b17951fd2def9d55c6b00f0d
Bao Dang <danglequocbao001@gmail.com>
2022-05-19 21:48:25 +0700

    fix: chore

---
+++ src/main/resources/static/scripts/news.js
@@ -0,0 +1,17 @@
+function incEltNbr(id) {
+  elt = document.getElementById(id);
+  endNbr = Number(document.getElementById(id).innerHTML);
+  incNbrRec(0, endNbr, elt);
+}
+
+function incNbrRec(i, endNbr, elt) {
+  var speed = 10;
+  if (i <= endNbr) {
+    elt.innerHTML = i;
+    setTimeout(function () {
+      incNbrRec(i + 1, endNbr, elt);
+    }, speed);
+  }
+}
+
+incEltNbr("nbr");
