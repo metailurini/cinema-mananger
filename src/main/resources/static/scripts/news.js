6c3c861d81f9005149f6737a4780cd8dba46ad8b
Bao Dang <danglequocbao001@gmail.com>
2022-05-21 02:17:53 +0700

    fix: chore route

--- src/main/resources/static/scripts/news.js
+++ src/main/resources/static/scripts/news.js
@@ -23,4 +23,9 @@ function openCloseDropdownUser() {
   }
 }
 
+function deleteAuthTokenCookie() {
+  document.cookie = "_auth_token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;'";
+  location.reload();
+}
+
 incEltNbr("nbr");
