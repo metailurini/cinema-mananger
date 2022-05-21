eb3a2772eb6c871f66d6a340084127759c6170c1
Bao Dang <danglequocbao001@gmail.com>
2022-05-21 17:48:30 +0700

    chore: check conflict

--- src/main/resources/static/scripts/revenue.js
+++ src/main/resources/static/scripts/revenue.js
@@ -1,17 +1,31 @@
-function incEltNbr(id) {
+function incEltNbr(id, isCustom) {
   elt = document.getElementById(id);
   endNbr = Number(document.getElementById(id).innerHTML);
-  incNbrRec(0, endNbr, elt);
+  incNbrRec(0, endNbr, elt, isCustom);
 }
 
-function incNbrRec(i, endNbr, elt) {
+function incNbrRec(i, endNbr, elt, isCustom) {
   var speed = 10;
   if (i <= endNbr) {
-    elt.innerHTML = i;
+    if (isCustom == false) {
+      elt.innerHTML = i;
+    } else {
+      elt.innerHTML = numberWithCommas(i);
+    }
     setTimeout(function () {
-      incNbrRec(i + 1, endNbr, elt);
+      if (isCustom == false) {
+        incNbrRec(i + 1, endNbr, elt, false);
+      } else {
+        incNbrRec(i + endNbr * 0.01, endNbr, elt, true);
+      }
     }, speed);
   }
+}
+
+function numberWithCommas(x) {
+  var parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  return parts
 }
 
 var child = document.getElementById("child");
@@ -21,4 +35,7 @@ for (let i = 0; i < 10; i++) {
   father.appendChild(child.cloneNode(true));
 }
 
-incEltNbr("nbr");
+incEltNbr("nbr", false);
+incEltNbr("nbrRevenueToday", true);
+incEltNbr("nbrRevenueThisMonth", true);
+incEltNbr("nbrRevenueThisYear", true);
