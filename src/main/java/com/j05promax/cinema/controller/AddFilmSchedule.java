cb6cdb2a00c75fb0b1c67bc96363d2adaac35cd0
Bao Dang <danglequocbao001@gmail.com>
2022-05-21 21:10:20 +0700

    feat: UI add film info

---
+++ src/main/java/com/j05promax/cinema/controller/AddFilmSchedule.java
@@ -0,0 +1,24 @@
+package com.j05promax.cinema.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AddFilmSchedule {
+    @GetMapping("add-film-schedule")
+    public String addFilmSchedule(Model model, HttpServletRequest request, HttpServletResponse response) {
+        Context ctx = new Context();
+        ctx.request = request;
+        ctx.response = response;
+
+        ctx = Midleware.Authenticate(ctx);
+        if (!ctx.SignedIn) {
+            return "redirect:/auth/login";
+        }
+        return "add-film-schedule";
+    }
+}
